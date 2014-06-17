1016e7c57c2d6c750eb2fe0a127af28cc5298964
Mostafa Mokhtar <ttmahdy19@hotmail.com>
2014-06-17 16:17:41 -0700

    Add args to main

--- src/spillingHashJoin/RunHashJoin.java
+++ src/spillingHashJoin/RunHashJoin.java
@@ -1,5 +1,8 @@
 package spillingHashJoin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public abstract class RunHashJoin {
 
 	public static void main(String[] args) throws Exception {
@@ -24,25 +27,63 @@ public abstract class RunHashJoin {
 		// 30GB TPCH row count but narrow flat files
 		// Orders 	has 45,000,000 	rows
 		// Lineitem has 179,958,156	rows
-		String probeSideFilePath = "//Users//mmokhtar//Downloads//FlatFiles//30G//narrowLineitem30G.tbl"; 
-		String buildSideFilePath = "//Users//mmokhtar//Downloads//FlatFiles//30G//narrowOrders30G.tbl";
 		
-		// Path for results file
-		String resultSetFilePath = "//Users//mmokhtar//Downloads//spillData//queryResults.csv";
+		String probeSideFilePath;
+		String buildSideFilePath;
+		String resultSetFilePath;
+		String spillFilesFolder;
+		
+		// Info for Hash table
+		int spillLevel = 0;
+		int numberOfHashBuckets 	= 8;				// This is the unit of Spilling
+		int estimateNumberOfRows 	= 5000000;			// CE is 10e6 for build size use to construct the bloom filter
+		int maxInmemoryRowCount  	= 2000000;	 		// 2500000 no spill 1994-01-02 and 1995-01-02 
 
-		// Path for spill files
-		String spillFilesFolder = "//Users//mmokhtar//Downloads//spillData//";
+		
+		if (args.length >= 3)
+		{
+			// Parse the args
+			buildSideFilePath = args[0];
+			probeSideFilePath = args[1];
+			spillFilesFolder  = args[2];
+			resultSetFilePath = args[3];
+		}
+		else
+		{
+			buildSideFilePath 	= "//Users//mmokhtar//Downloads//FlatFiles//30G//narrowOrders30G.tbl";
+			probeSideFilePath 	= "//Users//mmokhtar//Downloads//FlatFiles//30G//narrowLineitem30G.tbl";
+			spillFilesFolder 	= "//Users//mmokhtar//Downloads//spillData//";
+			resultSetFilePath 	= "//Users//mmokhtar//Downloads//spillData//queryResults.csv";
 
+		}
+
+		if (args.length ==6)
+		{
+			numberOfHashBuckets		= Integer.parseInt(args[4]);
+			estimateNumberOfRows	= Integer.parseInt(args[5]);
+			maxInmemoryRowCount		= Integer.parseInt(args[6]);
+		}
+		else
+		{
+			numberOfHashBuckets 	= 8;				// This is the unit of Spilling
+			estimateNumberOfRows 	= 5000000;			// CE is 10e6 for build size use to construct the bloom filter
+			maxInmemoryRowCount  	= 2000000;	 		// 2500000 no spill 1994-01-02 and 1995-01-02 
+		}
+		
+		File buildSideFile = new File(buildSideFilePath);
+		File probeSideFile = new File(probeSideFilePath);
+		
+		if (!buildSideFile.exists() || !probeSideFile.exists())
+		{
+			System.out.println("Usage : SpillingHashTable.jar probeSideFileName probeSideFileName spillFilesFolder resultSetFileName numberOfHashBuckets estimateNumberOfRows");
+			throw new FileNotFoundException("Files " + buildSideFilePath + " & " + probeSideFilePath + " not found!");
+		}
+		
 		// Filter on o_orderdate
 		String minDateFilter = "1994-01-02";
 		String maxDateFilter = "1995-01-02";
 		
-		// Info for Hash table
-		int numberOfHashBuckets 	= 8;				// This is the unit of Spilling
-		int estimateNumberOfRows 	= 5000000;			// CE is 10e6 for build size use to construct the bloom filter
-		int maxInmemoryRowCount  	= 2000000;	 		// 2500000 no spill 1994-01-02 and 1995-01-02 
-		int spillLevel 				= 0;				// Always start at 0, first time we spill level is 1, next time 2 etc..
-		
+		 
 		// TODO need to handle this correctly by keeping the hash tables size in check
 		int maxInMemorySize = Integer.MAX_VALUE; 	
 		
